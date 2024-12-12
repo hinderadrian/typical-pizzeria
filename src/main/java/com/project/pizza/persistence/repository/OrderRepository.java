449d4facf6ec2ae40d5340392be8def7ec786d64
Hinder Adrian Alvarez Perlaza <halvarezadrian@gmail.com>
2024-12-11 23:00:31 -0500

    Adding Order repository and service
    - Adding OrderRepository and OrderService

---
+++ src/main/java/com/project/pizza/persistence/repository/OrderRepository.java
@@ -0,0 +1,14 @@
+package com.project.pizza.persistence.repository;
+
+import com.project.pizza.persistence.entity.OrderEntity;
+import org.springframework.data.repository.ListCrudRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface OrderRepository extends ListCrudRepository<OrderEntity, Integer> {
+
+    List<OrderEntity> findAllByDateAfter(LocalDateTime date);
+
+    List<OrderEntity> findAllByMethodIn(List<String> methods);
+}
