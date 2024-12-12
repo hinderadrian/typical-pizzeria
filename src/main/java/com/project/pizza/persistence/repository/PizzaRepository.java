211fab029f18a5828b07cffcc7e8fe7bfef665ed
Hinder Adrian Alvarez Perlaza <halvarezadrian@gmail.com>
2024-12-11 22:53:40 -0500

    Adding Repository pageable & Service for Pizza
    - Adding PizzaPagSortRepository to handle request using pageable implementation and sorting too.

--- src/main/java/com/project/pizza/persistence/repository/PizzaRepository.java
+++ src/main/java/com/project/pizza/persistence/repository/PizzaRepository.java
@@ -4,14 +4,19 @@ import com.project.pizza.persistence.entity.PizzaEntity;
 import org.springframework.data.repository.ListCrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PizzaRepository extends ListCrudRepository<PizzaEntity, Integer> {
 
     List<PizzaEntity> findAllByAvailableTrueOrderByPrice();
 
-    PizzaEntity findAllByAvailableTrueAndNameIgnoreCase(String name);
+    Optional<PizzaEntity> findFirstAllByAvailableTrueAndNameIgnoreCase(String name);
 
     List<PizzaEntity> searchAllByAvailableTrueAndDescriptionContainsIgnoreCase(String description);
 
     List<PizzaEntity> searchAllByAvailableTrueAndDescriptionNotContainsIgnoreCase(String description);
+
+    List<PizzaEntity> findTop3ByAvailableTrueAndPriceLessThanEqualOrderByPriceAsc(double price);
+
+    int countAllByVeganTrue();
 }
--- src/main/java/com/project/pizza/service/PizzaService.java
+++ src/main/java/com/project/pizza/service/PizzaService.java
@@ -1,8 +1,13 @@
 package com.project.pizza.service;
 
 import com.project.pizza.persistence.entity.PizzaEntity;
+import com.project.pizza.persistence.repository.PizzaPagSortRepository;
 import com.project.pizza.persistence.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,19 +15,30 @@ import java.util.List;
 @Service
 public class PizzaService {
 
-    PizzaRepository pizzaRepository;
+    private final PizzaRepository pizzaRepository;
+    private final PizzaPagSortRepository pizzaPagSortRepository;
+
+
 
     @Autowired
-    public PizzaService(PizzaRepository pizzaRepository) {
+    public PizzaService(PizzaRepository pizzaRepository, PizzaPagSortRepository pizzaPagSortRepository) {
         this.pizzaRepository = pizzaRepository;
+        this.pizzaPagSortRepository = pizzaPagSortRepository;
     }
 
-    public List<PizzaEntity> getAllPizzas() {
-        return this.pizzaRepository.findAll();
+    public Page<PizzaEntity> getAllPizzas(int page, int elements) {
+        Pageable pageRequest = PageRequest.of(page, elements);
+        return this.pizzaPagSortRepository.findAll(pageRequest);
     }
 
-    public List<PizzaEntity> getAvailable() {
-        return this.pizzaRepository.findAllByAvailableTrueOrderByPrice();
+    public Page<PizzaEntity> getAvailable(int page, int elements, String sortBy, String sortDirection) {
+        Sort sortRequest = Sort.by(Sort.Direction.fromString(sortDirection), sortBy);
+        Pageable pageRequest = PageRequest.of(page, elements, sortRequest);
+
+        int count = this.pizzaRepository.countAllByVeganTrue();
+        System.out.println(count);
+        //return this.pizzaRepository.findAllByAvailableTrueOrderByPrice();
+        return this.pizzaPagSortRepository.findByAvailableTrue(pageRequest);
     }
 
     public PizzaEntity getPizzaById(int pizzaId) {
@@ -42,7 +58,8 @@ public class PizzaService {
     }
 
     public PizzaEntity getByName(String pizzaName) {
-        return this.pizzaRepository.findAllByAvailableTrueAndNameIgnoreCase(pizzaName);
+        return this.pizzaRepository.findFirstAllByAvailableTrueAndNameIgnoreCase(pizzaName)
+                .orElseThrow(() -> new RuntimeException("Pizza not found"));
     }
 
     public List<PizzaEntity> getWith(String ingredient) {
@@ -51,5 +68,9 @@ public class PizzaService {
 
     public List<PizzaEntity> getWithout(String ingredient) {
         return this.pizzaRepository.searchAllByAvailableTrueAndDescriptionNotContainsIgnoreCase(ingredient);
+    }
+
+    public List<PizzaEntity> getCheapest(double price) {
+        return this.pizzaRepository.findTop3ByAvailableTrueAndPriceLessThanEqualOrderByPriceAsc(price);
     }
 }
